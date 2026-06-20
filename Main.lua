e977d86d1dc04c66760d4709f7a37e41697d5050
VunixUser <krarhader748@gmail.com>
2026-06-20 09:53:22 +0300

    Implement library loading in Main.lua
    Add library loading functionality in Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,19 @@
+-- ملف Main.lua
+local Main = {}
 
+-- تعريف روابط المكتبة
+local AXIS_UI_URL = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/Window.lua"
+local BUTTON_URL = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/Button.lua"
+
+-- دالة تحميل المكتبات
+function Main.Load()
+    local AxisUI = loadstring(game:HttpGet(AXIS_UI_URL))()
+    local Button = loadstring(game:HttpGet(BUTTON_URL))()
+    
+    return {
+        Window = AxisUI,
+        Button = Button
+    }
+end
+
+return Main

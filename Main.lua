3eba155d96b021ceed34d9341abdf79d4428e4e6
VunixUser <krarhader748@gmail.com>
2026-06-20 21:42:21 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,16 +6,18 @@ local URLS = {
     Button   = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/Button.lua",
     Toggle   = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/Toggle.lua",
     Textbox  = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/Textbox.lua",
+    Section  = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/Section.lua", -- الموديول الجديد
     Gradient = "https://raw.githubusercontent.com/iamKernelK/Axis-UI/refs/heads/main/WindowGradient.lua"
 }
 
 function Main.Load()
-    -- تحميل الموديولات
+    -- تحميل جميع الموديولات بما في ذلك Section
     local WindowMod   = loadstring(game:HttpGet(URLS.Window))()
     local TabMod      = loadstring(game:HttpGet(URLS.Tab))()
     local ButtonMod   = loadstring(game:HttpGet(URLS.Button))()
     local ToggleMod   = loadstring(game:HttpGet(URLS.Toggle))()
     local TextboxMod  = loadstring(game:HttpGet(URLS.Textbox))()
+    local SectionMod  = loadstring(game:HttpGet(URLS.Section))() -- تحميل الـ Section
     local GradientMod = loadstring(game:HttpGet(URLS.Gradient))()
     
     return {
@@ -24,6 +26,7 @@ function Main.Load()
         Button   = ButtonMod,
         Toggle   = ToggleMod,
         Textbox  = TextboxMod,
+        Section  = SectionMod, -- إضافة Section للـ Return
         Gradient = GradientMod
     }
 end
